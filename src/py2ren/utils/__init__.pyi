0c1c6ca8cb39caa58d79ccf25420c1c5677e3c3f
Davila Yoimer <89022117+yoimerdr@users.noreply.github.com>
2025-01-11 17:25:48 -0500

    docs: add missing docstrings

--- src/py2ren/utils/__init__.pyi
+++ src/py2ren/utils/__init__.pyi
@@ -1,16 +1,41 @@
-from typing import Iterable, Type
-
-from . import filepath, iter
-from typing import TypeVar, Iterable, Callable, Generator
+from typing import TypeVar, Iterable, Callable, Type, MutableSequence
 
 _T = TypeVar("_T")
 _R = TypeVar("_R")
 
 
-def rowchars(text: str, char: str, start: int = 0) -> int: pass
+def rowchars(text: str, char: str, start: int = 0) -> int:
+    """
+    Counts the consecutive occurrences of a character.
+
+    :param text: The input text to search in.
+    :param char: The character to count occurrences of.
+    :param start: The starting position in the text.
+    :return: The number occurrences.
+    """
+    pass
 
 
-def strip_indexes(source: Iterable, indexes: Iterable[int]): pass
+def strip_indexes(source: MutableSequence, indexes: Iterable[int]):
+    """
+    Deletes the items of the source object at the specified indexes.
+
+    :param source: An object that allows `__delitem__`.
+    :param indexes: The indexes of elements to remove.
+    """
+    pass
 
 
-def trymap(func: Callable[[_T], _R], value: _T, default: _R = None, allow: Iterable[Type[Exception]] = None): pass
+def trymap(func: Callable[[_T], _R], value: _T, default: _R = None, allow: Iterable[Type[Exception]] = None) -> _R:
+    """
+    Safely apply a function to a value.
+
+    If the `allow` param is None, all exceptions are allowed.
+
+    :param func: The function to apply to the value.
+    :param value:  The value to pass to the function.
+    :param default: The value to return if an exception occurs.
+    :param allow: Types of exceptions to catch and handle.
+    :return: The result of func(value), or default if an allowed exception occurs.
+    """
+    pass
